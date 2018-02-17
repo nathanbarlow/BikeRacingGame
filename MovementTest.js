259656335afdf9460ce9a7dcc90f9f7fc62fedb2
nathanbarlow <31486268+nathanbarlow@users.noreply.github.com>
2018-02-16 21:07:57 -0700

    Add files via upload

--- MovementTest.js
+++ MovementTest.js
@@ -66,7 +66,7 @@ var particles = new THREE.Geometry();
 
 //add particles to geometry
 for (var p = 0; p < 500; p++){
-	// (3600, -78, 471); character start position
+	// (3600, -78, 471); player.collision start position
 	var particle = new THREE.Vector3(Math.random()*50 + 3600, Math.random()*300, Math.random()*50 + 500);
 	particles.vertices.push(particle);
 }
@@ -230,62 +230,153 @@ function handle_load_racetrack(loadObject) {
 	// scene.add(raceTrackCollision);
 }
 
-//BLENDER MODEL BIKE LOAD
-//OBJECT Hierarchy: charMesh( charModel( charMeshWheel, charMeshSeat ) )
+var player = {
+	globalLevel	: new THREE.Group(),
+	localLevel	: new THREE.Group(),
+	seat				: new THREE.Mesh(),
+	wheel				: new THREE.Mesh(),
+	texture			: new THREE.TextureLoader().load('Bike.png'),
+	collision		: "set up in player.init",
+	speedTurnInfluence: function(){return 0.0000008 * player.collision.speed},
+	turnResponsiveness: function(){return 0.0007 - player.speedTurnInfluence()},
+	maxTurn						: 0.025,
 
-var loader = new THREE.JSONLoader(); //used for all blender model imports
-var charMesh = new THREE.Group();
-var charModel = new THREE.Group();
-var charMeshSeat = new THREE.Mesh();
-var charMeshWheel = new THREE.Mesh();
-var bikeTexture = new THREE.TextureLoader().load('Bike.png');
+	sync: function(){
+		player.globalLevel.position.x = player.collision.position.x;
+	  player.globalLevel.position.z = player.collision.position.y;
+	  player.globalLevel.rotation.y = -player.collision.angle;
+	},
 
-//load Seat
-loader.load('BikeSeat.json', handle_load_seat);
-var thing;
-function handle_load_seat(loadObject) {
-	charMeshSeat.geometry = loadObject;
-	let mat = new THREE.MeshLambertMaterial({
-		map: bikeTexture,
-	});
-	charMeshSeat.material = mat;
-	charMeshSeat.scale.set(50, 50, 50);
-}
+	turnLeft: function(){
+		let velocity = Math.max(player.collision.angularVelocity - player.turnResponsiveness(), -player.maxTurn);
+		Matter.Body.setAngularVelocity(player.collision, velocity);
 
-//load Wheel
-loader.load('BikeWheel.json', handle_load_wheel)
-function handle_load_wheel(loadObject) {
-	charMeshWheel.geometry = loadObject;
-	let mat = new THREE.MeshLambertMaterial({
-		map: bikeTexture,
-	});
-	charMeshWheel.material = mat;
-	charMeshWheel.scale.set(50, 50, 50);
-}
+		//Rotate bike to lean in to turn
+		playerTurning = true;
+	},
 
-//set charModel rotation offset
-//(allows bike to lean left and right and rotate arround bottom of tire)
-var charModelCenterOffset = 140;
-charMeshWheel.position.y = charModelCenterOffset;
-charMeshSeat.position.y = charModelCenterOffset;
-charModel.position.y = -charModelCenterOffset;
+	turnRight: function(){
+		let velocity = Math.min(player.collision.angularVelocity + player.turnResponsiveness(), player.maxTurn);
+		Matter.Body.setAngularVelocity(player.collision, velocity);
 
-//Set position of Bike in world
-charMesh.position.set(3600, -78, 471); //0, -200, -500
+		//Rotate bike to lean in to turn
+		playerTurning = true;
+	},
 
-//Setup Group Hierarchy and add to scene
-charModel.add( charMeshSeat );
-charModel.add( charMeshWheel );
-charMesh.add( charModel );
-scene.add( charMesh );
+	moveForward: function(){
+		let localForceX = 0;
+		let localForceY = (-0.007 * player.collision.mass);
 
-var character = Matter.Bodies.rectangle(charMesh.position.x, charMesh.position.z, 130, 440, {
-  friction: 0.0001,
-  frictionAir: 0.012,
-	mass: 500,
-});
+		//Figures out what local forc needs to be on vehicle
+		let globalForce = globalXYFromLocalXY(localForceX, localForceY, player.collision);
 
-//ADD STATIC FLOOR
+		Matter.Body.applyForce(player.collision,player.collision.position,{
+			x:globalForce.x,
+			y:globalForce.y
+		});
+	},
+
+	moveBackward: function(){
+		let localForceX = 0;
+		let localForceY = (+0.001 * player.collision.mass);
+
+		//Figures out what local forc needs to be on vehicle
+		let globalForce = globalXYFromLocalXY(localForceX, localForceY, player.collision);
+
+		Matter.Body.applyForce(player.collision,player.collision.position,{
+			x:globalForce.x,
+			y:globalForce.y
+		});
+	},
+
+	LeanHandler: function(){
+		var rotationFactor = 35; //degrees
+		var maxAngularVelocity = 0.025;
+		var maxPossibleRotation = degToRad(35); //radions
+
+		//BIKE Lean handler
+		player.localLevel.rotation.z = degToRad(-player.collision.angularVelocity * rotationFactor / maxAngularVelocity);
+
+		if (player.localLevel.rotation.z > maxPossibleRotation) {
+			player.localLevel.rotation.z = maxPossibleRotation;
+		}
+
+		if (player.localLevel.rotation.z < -maxPossibleRotation) {
+			player.localLevel.rotation.z = -maxPossibleRotation;
+		}
+
+		//Reset playerTurning variable
+		playerTurning = false;
+	},
+
+	wheelRotationHandler: function() {
+		speedFactor = 0.09;
+
+		vel = getObjectsLocalVelocity(player.collision);
+		if(vel.y < 0){
+			speedFactor *= -1;
+		} else {
+			speedFactor *= 1;
+		}
+		player.wheel.rotateX(degToRad(player.collision.speed * speedFactor));
+	},
+
+	init: function(){
+		//BLENDER MODEL BIKE LOAD
+		//OBJECT Hierarchy: player.globalLevel( player.localLevel( player.wheel, player.seat ) )
+		var loader = new THREE.JSONLoader(); //used for all blender model imports
+		//load Seat
+		loader.load('BikeSeat.json', handle_load_seat);
+
+		function handle_load_seat(loadObject) {
+			player.seat.geometry = loadObject;
+			let mat = new THREE.MeshLambertMaterial({
+				map: player.texture,
+			});
+			player.seat.material = mat;
+			player.seat.scale.set(50, 50, 50);
+		}
+
+		//load Wheel
+		loader.load('BikeWheel.json', handle_load_wheel)
+		function handle_load_wheel(loadObject) {
+			player.wheel.geometry = loadObject;
+			let mat = new THREE.MeshLambertMaterial({
+				map: player.texture,
+			});
+			player.wheel.material = mat;
+			player.wheel.scale.set(50, 50, 50);
+		}
+
+		//set player.localLevel rotation offset
+		//(allows bike to lean left and right and rotate arround bottom of tire)
+		var charModelCenterOffset = 140;
+		player.wheel.position.y = charModelCenterOffset;
+		player.seat.position.y = charModelCenterOffset;
+		player.localLevel.position.y = -charModelCenterOffset;
+
+		//Set position of Bike in world
+		player.globalLevel.position.set(3600, -78, 471); //0, -200, -500
+
+		//Setup Group Hierarchy and add to scene
+		player.localLevel.add( player.seat );
+		player.localLevel.add( player.wheel );
+		player.globalLevel.add( player.localLevel );
+		scene.add( player.globalLevel );
+
+		//Set up player collision with Matter.js
+		player.collision = Matter.Bodies.rectangle(player.globalLevel.position.x, player.globalLevel.position.z, 130, 440, {
+			friction: 0.0001,
+			frictionAir: 0.012,
+			mass: 500,
+		});
+	}
+};
+
+player.init();
+
+
+//ADD STATIC OBJECT
 var baseGeometry = new THREE.BoxGeometry(1000, 100, 100);
 var baseMaterial = new THREE.MeshLambertMaterial({
 	color: 0xffffff,
@@ -297,40 +388,25 @@ scene.add( baseMesh );
 
 var boxB = Matter.Bodies.rectangle(baseMesh.position.x, baseMesh.position.z, 1000, 100,{ isStatic: true });
 
-//ADD EARTH GROUND
-// var textureEarth = new THREE.TextureLoader().load( 'ground.jpg', function ( textureEarth ) {
-//     textureEarth.wrapS = textureEarth.wrapT = THREE.RepeatWrapping;
-//     textureEarth.offset.set( 0, 0 );
-//     textureEarth.repeat.set( 15, 15 );
-// } );
-//
-// var groundGeometry = new THREE.PlaneGeometry(70000, 70000, 20);
-// var groundMaterial = new THREE.MeshLambertMaterial({
-// 	map: textureEarth,
-// 	// side: THREE.DoubleSide,
-// });
-//
-// var groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
-// groundMesh.position.set(0, -250, 0);
-// groundMesh.rotation.x = (degToRad(-90));
-// scene.add( groundMesh );
 
-//ADD STATIC FLOOR2
+//ADD STATIC OBJECT_2
 var baseMesh2 = new THREE.Mesh(baseGeometry, baseMaterial);
 baseMesh2.position.set(0, -200, 400);
 scene.add( baseMesh2 );
 
 var boxC = Matter.Bodies.rectangle(baseMesh2.position.x, baseMesh2.position.z, 1000, 100,{ isStatic: true });
 
+
 //CAMERA
 camera = new THREE.PerspectiveCamera( 45.0, window.innerWidth / window.innerHeight, 0.1, 50000 );
-camera.position.set( character.position.x, 600, -1000 );
+camera.position.set( player.collision.position.x, 600, -1000 );
 var cameraTargetPosition = new THREE.Vector3();
 var cameraDelay = 10;
 
+
 //ADD MOVEMENT CONTROLS
 // controls = new THREE.OrbitControls( camera );
-// controls.target.set( charMesh.position.x, charMesh.position.y, charMesh.position.z);
+// controls.target.set( player.globalLevel.position.x, player.globalLevel.position.y, player.globalLevel.position.z);
 // controls.update();
 // controls.enabled = true;
 
@@ -340,8 +416,9 @@ Matter.World.add(engine.world, [
 	boxA,
 	boxB,
 	boxC,
-	character,
+	player.collision,
 	]);
+
 
 //ADJUST GRAVITY
 engine.world.gravity.y = 0;
@@ -370,8 +447,8 @@ function render() {
 
 	var viewZoom = 8000;
 	Matter.Render.lookAt(render2D, {
-	        min: { x: character.position.x - viewZoom, y: character.position.y - viewZoom },
-	        max: { x: character.position.x + viewZoom, y: character.position.y + viewZoom }
+	        min: { x: player.collision.position.x - viewZoom, y: player.collision.position.y - viewZoom },
+	        max: { x: player.collision.position.x + viewZoom, y: player.collision.position.y + viewZoom }
 	    });
   //write the cameras position to the console every 30 frames
   // frameCount += 1
@@ -391,87 +468,55 @@ function render() {
   baseMesh2.position.x = boxC.position.x;
   baseMesh2.position.z = boxC.position.y;
 
-  charMesh.position.x = character.position.x;
-  charMesh.position.z = character.position.y;
-  charMesh.rotation.y = -character.angle;
+  player.sync();
 
 	//KEY INPUT
   if((keys[KEY_S])){
-      let localForceX = 0;
-      let localForceY = (+0.001 * character.mass);
-
-			//Figures out what local forc needs to be on vehicle
-			let globalForce = globalXYFromLocalXY(localForceX, localForceY, character);
-
-      Matter.Body.applyForce(character,character.position,{
-        x:globalForce.x,
-        y:globalForce.y
-      });
-			//charMesh.rotation.z = degToRad(10);
+			player.moveBackward();
   }
 
 	if((keys[KEY_W])){
-      let localForceX = 0;
-      let localForceY = (-0.007 * character.mass);
-
-			//Figures out what local forc needs to be on vehicle
-			let globalForce = globalXYFromLocalXY(localForceX, localForceY, character);
-
-      Matter.Body.applyForce(character,character.position,{
-        x:globalForce.x,
-        y:globalForce.y
-      });
+		player.moveForward();
   }
-	// var speedTurnInfluence = Math.max(0.001,0.00001 * character.speed);
-	var speedTurnInfluence = 0.0000008 * character.speed;
-	var turnResponsiveness = 0.0007 - speedTurnInfluence;
-	var maxTurn = 0.025;
-  if((keys[KEY_A])){
-      let velocity = Math.max(character.angularVelocity - turnResponsiveness, -maxTurn);
-      Matter.Body.setAngularVelocity(character, velocity);
 
-			//Rotate bike to lean in to turn
-			playerTurning = true;
+  if((keys[KEY_A])){
+  	player.turnLeft();
   }
 
   if((keys[KEY_D])){
-      let velocity = Math.min(character.angularVelocity + turnResponsiveness, maxTurn);
-      Matter.Body.setAngularVelocity(character, velocity);
-
-			//Rotate bike to lean in to turn
-			playerTurning = true;
+		player.turnRight();
   }
 
 	//Update the lean of the bike
-	bikeLeanHandler();
+	player.LeanHandler();
 
-	//Rotate Wheel based on character objects velocity
-	wheelRotationHandler();
+	//Rotate Wheel based on player.collision objects velocity
+	player.wheelRotationHandler();
 
 	//Update Lava Material
 	textureLava.offset.y += 0.0005;
 
 	//Turn Sideways velocity into forward velocity/Kill sideways velocity
-	var charVelocity = getObjectsLocalVelocity(character);
+	var charVelocity = getObjectsLocalVelocity(player.collision);
 	//check x axis velocity
 	if(Math.abs(charVelocity.x) >= 0.1){
 		//Kill x axis velocity
-		let globalVel = globalXYFromLocalXY(0, charVelocity.y, character);
-		Matter.Body.setVelocity(character, globalVel);
+		let globalVel = globalXYFromLocalXY(0, charVelocity.y, player.collision);
+		Matter.Body.setVelocity(player.collision, globalVel);
 	}
 
   //Update Camera position with delay
 	cameraTargetPosition.set(
-		charMesh.position.x + cameraRelative.x * Math.cos(character.angle) - cameraRelative.z * Math.sin(character.angle),
+		player.globalLevel.position.x + cameraRelative.x * Math.cos(player.collision.angle) - cameraRelative.z * Math.sin(player.collision.angle),
     cameraRelative.y,
-    charMesh.position.z + cameraRelative.x * Math.sin(character.angle) + cameraRelative.z * Math.cos(character.angle)
+    player.globalLevel.position.z + cameraRelative.x * Math.sin(player.collision.angle) + cameraRelative.z * Math.cos(player.collision.angle)
 	);
 
 	if (camera.position.distanceTo(cameraTargetPosition) >= cameraDelay) {
 		camera.position.lerp(cameraTargetPosition, 0.1);
 	}
 
-  camera.lookAt( charMesh.position );
+  camera.lookAt( player.globalLevel.position );
 
 	//UPDATE DISPLAYED INFO
 	updateSpeedometer();
@@ -510,42 +555,10 @@ function globalXYFromLocalXY(forceX, forceY, inputBody){
 	return {x:globalX, y:globalY};
 }
 
-function bikeLeanHandler() {
-	var rotationFactor = 35; //degrees
-	var maxAngularVelocity = 0.025;
-	var maxPossibleRotation = degToRad(35); //radions
-
-	//BIKE Lean handler
-	charModel.rotation.z = degToRad(-character.angularVelocity * rotationFactor / maxAngularVelocity);
-
-	if (charModel.rotation.z > maxPossibleRotation) {
-		charModel.rotation.z = maxPossibleRotation;
-	}
-
-	if (charModel.rotation.z < -maxPossibleRotation) {
-		charModel.rotation.z = -maxPossibleRotation;
-	}
-
-	//Reset playerTurning variable
-	playerTurning = false;
-}
-
-function wheelRotationHandler() {
-	speedFactor = 0.09;
-
-	vel = getObjectsLocalVelocity(character);
-	if(vel.y < 0){
-		speedFactor *= -1;
-	} else {
-		speedFactor *= 1;
-	}
-	charMeshWheel.rotateX(degToRad(character.speed * speedFactor));
-}
-
 function updateSpeedometer(){
 	renderCount += 1;
 	if (renderCount >= 30){
-		document.getElementById("speedometer").innerHTML = character.speed.toFixed(0);
+		document.getElementById("speedometer").innerHTML = player.collision.speed.toFixed(0);
 		renderCount = 0;
 	};
 }
